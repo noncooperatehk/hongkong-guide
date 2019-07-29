de520eb5bf080fa48920044e4c9900380b1e0ccd
noncooperatehk <53223012+noncooperatehk@users.noreply.github.com>
2019-07-29 23:53:22 +0800

    fix warning

--- src/components/SearchCompany.vue
+++ src/components/SearchCompany.vue
@@ -7,7 +7,7 @@
         <md-input v-model="searchValue" placeholder="type names to search... like yoshinoya" v-on:keyup.enter="doSearch($event)"></md-input>
       </md-field> 
     </div>
-    <CompanyPreview v-for="companyPreview in companyPreviews" v-bind:preview="companyPreview"></CompanyPreview>
+    <CompanyPreview v-for="(companyPreview,index) in companyPreviews" v-bind:preview="companyPreview" v-bind:key="index"></CompanyPreview>
   </div>
 </template>
 
