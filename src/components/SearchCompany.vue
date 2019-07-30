64084ac707e25b9e4147a2ffc88591ef6edb147d
noncooperatehk <53223012+noncooperatehk@users.noreply.github.com>
2019-07-30 19:09:08 +0800

    add an extra layout to make cards vertically align with the search bar

--- src/components/SearchCompany.vue
+++ src/components/SearchCompany.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="md-layout md-alignment-top-center">
         <h1 class="md-layout-item md-large-size-80 md-xlarge-size-80 md-medium-size-85 md-small-size-90 md-xsmall-size-95">{{ msg }}</h1>
-        <div class="md-layout-item md-large-size-80 md-xlarge-size-80 md-medium-size-85 md-small-size-90 md-xsmall-size-95">
-            <md-field>
-                <label>搜尋商家</label>
-                <md-input v-model="searchValue" placeholder="請輸入商家名字"
-                          v-on:keyup.enter="doSearch($event)"></md-input>
-            </md-field>
+        <div class="md-layout md-layout-item md-large-size-80 md-xlarge-size-80 md-medium-size-85 md-small-size-90 md-xsmall-size-95 md-gutter">
+            <div class="md-layout-item">
+                <md-field>
+                    <label>搜尋商家</label>
+                    <md-input v-model="searchValue" placeholder="請輸入商家名字" v-on:keyup.enter="doSearch($event)"></md-input>
+                </md-field>
+            </div>
         </div>
         <div class="md-layout-item md-alignment-top-left md-large-size-80 md-xlarge-size-80 md-medium-size-85 md-small-size-90 md-xsmall-size-95 md-layout md-gutter">
             <CompanyPreview class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100" v-for="(companyPreview,index) in companyPreviews" v-bind:preview="companyPreview" v-bind:key="index"/>
